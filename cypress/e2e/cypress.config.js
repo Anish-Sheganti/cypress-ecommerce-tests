a9a4302a37c42f2892f269511cbd3beeafe47c45
Kavya Sri <kadambari1102000@gmail.com>
2025-05-14 15:15:35 +0530

    Remove duplicate cypress.config.js from e2e folder

--- cypress/e2e/cypress.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    specPattern: "cypress/e2e/**/*.cy.js",  // This tells Cypress where to find the test files
-  },
-});
